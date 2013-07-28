c1a4759cb0886af27f116beb2d9d120bb01742fe
Johann Heyne <mail@johannheyne.de>
2013-07-28 22:31:19 +0200

    fix whitespace

--- wordpress/wp-content/themes/theme/header.php
+++ wordpress/wp-content/themes/theme/header.php
@@ -4,12 +4,12 @@
     
 ?><!DOCTYPE html>
 <head>
-	<meta charset="utf-8"/>
-	<title></title>
-	<!-- Adaptive-Images Cookie -->
-  	<script>document.cookie='resolution='+Math.max(screen.width,screen.height)+("devicePixelRatio" in window ? ","+devicePixelRatio : ",1")+'; path=/';</script>
+    <meta charset="utf-8"/>
+    <title></title>
+    <!-- Adaptive-Images Cookie -->
+    <script>document.cookie='resolution='+Math.max(screen.width,screen.height)+("devicePixelRatio" in window ? ","+devicePixelRatio : ",1")+'; path=/';</script>
 
-	<!-- meta, styles and other scripts goes here -->
+    <!-- meta, styles and other scripts goes here -->
 
 </head>
     
