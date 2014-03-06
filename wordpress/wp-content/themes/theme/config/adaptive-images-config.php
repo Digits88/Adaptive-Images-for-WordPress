0ca03a066c33eb699c1e9ae5206866268ff55915
johannheyne <mail@johannheyne.de>
2014-03-06 14:05:00 +0100

    changed spaces to tabs

--- wordpress/wp-content/themes/theme/config/adaptive-images-config.php
+++ wordpress/wp-content/themes/theme/config/adaptive-images-config.php
@@ -1,121 +1,111 @@
 <?php
-    
-    if ( isset( $themepath ) ) {
-    
-        include( $themepath . 'config/config.php');
-    
-        $config['cache_path']           = 'ai-cache'; 
-        $config['jpg_quality']          = 95; // 100 to 0
-        $config['jpg_quality_retina']   = 50; // 100 to 0
-        $config['watch_cache']          = TRUE;
-        $config['browser_cache']        = 60 * 60 * 24; // period of time in second, the images will stay in cache of browsers
-        $config['prevent_cache']        = FALSE; // images will resized on every image request
-        $config['debug_mode']           = FALSE; // insert the image dimensions, the ratio and the device-width into the image
-        $config['sharpen']['status']    = TRUE; // enables sharpen
-        $config['sharpen']['amount']    = 20; // 0 is none, 30 is pleasant, max is 500
-    
-        if ( config_site_is_type( 'test' ) ) {
-    
-            $config['browser_cache']    = 1; // period of time in second, the images will stay in cache of browsers
-            $config['prevent_cache']    = TRUE; // images will resized on every image request
-            $config['debug_mode']       = TRUE; // insert the image dimensions, the filesize and the ratio
-        }
-    }
-    
-    $config['resolutions'] = array(
-        'mobile' => 0, 
-        'medium' => 1000,
-        'large' => 1300
-    );
-    
-    /* Documentation
-        
-        Defining the image-width relating to resolution
-    
-            // resolution by value:
-            $setup['name']['resolutions'][0]['w'] = 300;
-            $setup['name']['resolutions'][1000]['w'] = 600;
-    
-            // resolution by $config['resolutions'] names:
-            $setup['name']['resolutions']['mobile']['w'] = 300;
-            $setup['name']['resolutions']['medium']['w'] = 600;
-    
-    
-        We can also define a height only
-    
-            $setup['name']['resolutions']['large']['h'] = 400;
-    
-    
-        We can also define width and height
-        
-            $setup['name']['resolutions']['large']['w'] = 400;
-            $setup['name']['resolutions']['large']['h'] = 400;
-    
-    
-        We can also define a aspect-ratio depending on a width or height
-    
-            $setup['name']['ratio'] = '2:1';
-    
-    
-        Defining the jpeg-quality
-    
-            $setup['name']['jpg_quality'] = 95;
-            $setup['name']['jpg_quality_retina'] = 40;
-    
-    
-        Defining the amount of sharpening
-    
-            $setup['name']['sharpen']['amount'] = 40;
-            
-            
-        Disabling retina
-        
-            We can disable the retina because in some cases, the image will be larger 
-            then the max size that a device can handle.
-            http://www.williammalone.com/articles/html5-javascript-ios-maximum-image-size/
-        
-            $setup['name']['retina'] = false;
-            
-            
-        Image filters
-        
-            Now we can add multiple php image filters.
-            http://php.net/manual/en/function.imagefilter.php
-            http://www.phpied.com/image-fun-with-php-part-2
-            
-            $setup['name']['filter'][IMG_FILTER_GRAYSCALE] = true;
-            $setup['name']['filter'][IMG_FILTER_COLORIZE] = array(0, 0, 100);
-            
-            
-        Cropping Rules
 
-            Now we can change the way cropping behaves.
-            Default value is false and causes centered cropping (center center)
-            "top" causes (top center) and is good for website screenshots
-            More cropping options will follow.
-            
-            $setup['name']['crop']['behavior'] = 'top';
-        
-        
-        Handling images with to low resolution
-            
-            If an image has a lower resolution than the AI result would be,
-            there are two options:
-                
-                false: the image will be skaled up
-                'preserve-size': the size of the image will be preserved
-                
-            $setup['name']['lowres'] = false;
-        
-        
-        Handling max-width of images
+	if ( isset( $themepath ) ) {
 
-            If an image should not displayed wider than it is defined in the resolution
-            settings.
+		include( $themepath . 'config/config.php');
 
-            $setup['name']['use-max-width'] = true; 
-     */
-     
-     $setup['zoom']['resolutions']['mobile']['w'] = 960;
-     
+		$config['cache_path']			= 'ai-cache'; 
+		$config['jpg_quality']			= 95; // 100 to 0
+		$config['jpg_quality_retina']	= 50; // 100 to 0
+		$config['watch_cache']			= TRUE;
+		$config['browser_cache']		= 60 * 60 * 24; // period of time in second, the images will stay in cache of browsers
+		$config['prevent_cache']		= FALSE; // images will resized on every image request
+		$config['debug_mode']			= FALSE; // insert the image dimensions, the ratio and the device-width into the image
+		$config['sharpen']['status']	= TRUE; // enables sharpen
+		$config['sharpen']['amount']	= 20; // 0 is none, 30 is pleasant, max is 500
+
+		if ( config_site_is_type( 'test' ) ) {
+
+			$config['browser_cache']	= 1; // period of time in second, the images will stay in cache of browsers
+			$config['prevent_cache']	= TRUE; // images will resized on every image request
+			$config['debug_mode']		= TRUE; // insert the image dimensions, the filesize and the ratio
+		}
+	}
+
+	$config['resolutions'] = array(
+		'mobile' => 0, 
+		'medium' => 1000,
+		'large' => 1300
+	);
+
+	/* Documentation
+
+		Defining the image-width relating to resolution
+
+			// resolution by value:
+			$setup['name']['resolutions'][0]['w'] = 300;
+			$setup['name']['resolutions'][1000]['w'] = 600;
+
+			// resolution by $config['resolutions'] names:
+			$setup['name']['resolutions']['mobile']['w'] = 300;
+			$setup['name']['resolutions']['medium']['w'] = 600;
+
+		We can also define a height only
+
+			$setup['name']['resolutions']['large']['h'] = 400;
+
+		We can also define width and height
+
+			$setup['name']['resolutions']['large']['w'] = 400;
+			$setup['name']['resolutions']['large']['h'] = 400;
+
+		We can also define a aspect-ratio depending on a width or height
+
+			$setup['name']['ratio'] = '2:1';
+
+		Defining the jpeg-quality
+
+			$setup['name']['jpg_quality'] = 95;
+			$setup['name']['jpg_quality_retina'] = 40;
+
+		Defining the amount of sharpening
+
+			$setup['name']['sharpen']['amount'] = 40;
+
+		Disabling retina
+
+			We can disable the retina because in some cases, the image will be larger 
+			then the max size that a device can handle.
+			http://www.williammalone.com/articles/html5-javascript-ios-maximum-image-size/
+
+			$setup['name']['retina'] = false;
+
+		Image filters
+
+			Now we can add multiple php image filters.
+			http://php.net/manual/en/function.imagefilter.php
+			http://www.phpied.com/image-fun-with-php-part-2
+
+			$setup['name']['filter'][IMG_FILTER_GRAYSCALE] = true;
+			$setup['name']['filter'][IMG_FILTER_COLORIZE] = array(0, 0, 100);
+
+		Cropping Rules
+
+			Now we can change the way cropping behaves.
+			Default value is false and causes centered cropping (center center)
+			"top" causes (top center) and is good for website screenshots
+			More cropping options will follow.
+
+			$setup['name']['crop']['behavior'] = 'top';
+
+		Handling images with to low resolution
+
+			If an image has a lower resolution than the AI result would be,
+			there are two options:
+
+				false: the image will be skaled up
+				'preserve-size': the size of the image will be preserved
+
+			$setup['name']['lowres'] = false;
+
+		Handling max-width of images
+
+			If an image should not displayed wider than it is defined in the resolution
+			settings.
+
+			$setup['name']['use-max-width'] = true; 
+	 */
+
+	 $setup['zoom']['resolutions']['mobile']['w'] = 960;
+
 ?>
--- wordpress/wp-content/themes/theme/config/config.php
+++ wordpress/wp-content/themes/theme/config/config.php
@@ -1,39 +1,39 @@
 <?php
-    
-    /* configurate the domains für develepment and produktion mode */
-    
-    function config_data() {
-        
-        $config['domains'][$_SERVER["HTTP_HOST"]] = 'live';
-        $config['domains']['test.domain.com'] = 'test';
-        
-        return $config;
-    }
-    
-    /* please do not change the code below */
-    
-    function config_get_curr_blog_id () {
-         
-         global $current_blog;
 
-         if ( isset( $current_blog ) ) {
-             return $current_blog->blog_id;
-         }
-         else {
-             return 1;
-         }
-    }
-    
-    function config_site_is_type( $type ) {
-        
-        $config = config_data();
-        
-        if ( isset( $config['domains'][ $_SERVER["HTTP_HOST"] ] ) && $config['domains'][ $_SERVER["HTTP_HOST"] ] == $type ) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    
+	/* configurate the domains für develepment and produktion mode */
+
+	function config_data() {
+
+		$config['domains'][$_SERVER["HTTP_HOST"]] = 'live';
+		$config['domains']['test.domain.com'] = 'test';
+
+		return $config;
+	}
+
+	/* please do not change the code below */
+
+	function config_get_curr_blog_id () {
+
+		 global $current_blog;
+
+		 if ( isset( $current_blog ) ) {
+			 return $current_blog->blog_id;
+		 }
+		 else {
+			 return 1;
+		 }
+	}
+
+	function config_site_is_type( $type ) {
+
+		$config = config_data();
+
+		if ( isset( $config['domains'][ $_SERVER["HTTP_HOST"] ] ) && $config['domains'][ $_SERVER["HTTP_HOST"] ] == $type ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
 ?>
